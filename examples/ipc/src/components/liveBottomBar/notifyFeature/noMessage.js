3b3b5976d432a9731e1e1770bed86d3615a8cda2
tiankun <tiankun@tuya.com>
2020-07-01 18:28:42 +0800

    fix: fix notitify message

--- examples/ipc/src/components/liveBottomBar/notifyFeature/noMessage.js
+++ examples/ipc/src/components/liveBottomBar/notifyFeature/noMessage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View, StyleSheet, Image } from 'react-native';
 import { TYText } from 'tuya-panel-kit';
 import Strings from '../../../i18n';
@@ -8,16 +9,22 @@ import Config from '../../../config';
 const { cx, cy } = Config;
 
 class NoMessage extends React.Component {
+  static propTypes = {
+    tabContentHeight: PropTypes.number.isRequired,
+  };
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
+    const { tabContentHeight } = this.props;
     return (
-      <View style={styles.noMessagePage}>
+      <View style={[styles.noMessagePage, { height: tabContentHeight }]}>
         <Image source={Res.notify.noNewsImg} style={styles.noDataImg} />
         <View style={styles.tipBox}>
-          <TYText numberOfLines={1} style={styles.tipText}>{Strings.getLang('ipc_message_none')}</TYText>
+          <TYText numberOfLines={1} style={styles.tipText}>
+            {Strings.getLang('ipc_message_none')}
+          </TYText>
         </View>
       </View>
     );
