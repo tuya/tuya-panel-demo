0a9b251fa69075b6980df0a456dd8a1bc3801028
ShinyLeee <lshinylee007@gmail.com>
2020-12-12 17:25:50 +0800

    feat(airThermostat): add air thermostat template

--- examples/basic-ts-0.59/.eslintrc.js
+++ examples/basic-ts-0.59/.eslintrc.js
@@ -1,78 +1,27 @@
+const path = require('path');
+
 module.exports = {
   root: true,
-  extends: ['./.eslintrc.base.js'],
+  extends: ['tuya/react-native'],
   rules: {
-    'import/no-unresolved': ['error', { ignore: ['^(react|react-native)$'] }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import/no-extraneous-dependencies': 0,
-    'react/jsx-filename-extension': 0,
-    'react/sort-comp': [
-      1,
-      {
-        order: [
-          'static-variables',
-          'static-methods',
-          'lifecycle',
-          '/^on.+$/',
-          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-          'everything-else',
-          '/^render.+$/',
-          'render',
-        ],
-      },
-    ],
+    'one-var': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'react/jsx-props-no-spreading': 0,
   },
   settings: {
-    'import/core-modules': ['@tuya-rn/tuya-native-web-context'],
     'import/resolver': {
       alias: {
         map: [
-          ['@api', './src/api'],
-          ['@components', './src/components'],
-          ['@config', './src/config'],
-          ['@i18n', './src/i18n'],
-          ['@models', './src/models'],
-          ['@res', './src/res'],
-          ['@utils', './src/utils'],
+          ['@api', path.resolve(__dirname, './src/api')],
+          ['@components', path.resolve(__dirname, './src/components')],
+          ['@config', path.resolve(__dirname, './src/config')],
+          ['@i18n', path.resolve(__dirname, './src/i18n')],
+          ['@models', path.resolve(__dirname, './src/models')],
+          ['@res', path.resolve(__dirname, './src/res')],
+          ['@utils', path.resolve(__dirname, './src/utils')],
         ],
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
       },
-      'react-native': { platform: 'both' },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.ios.js', '.android.js'],
-      },
     },
   },
-  overrides: [
-    {
-      parser: '@typescript-eslint/parser',
-      files: ['**/*.ts', '**/*.tsx'],
-      env: { browser: true, es6: true, node: true },
-      extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: ['react', 'react-native', 'prettier', '@typescript-eslint'],
-      rules: {
-        'react/prop-types': 0,
-        'react/display-name': 0,
-        '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/no-use-before-define': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/ban-ts-ignore': 0,
-      },
-    },
-  ],
 };
--- examples/basic-ts-0.59/src/main.tsx
+++ examples/basic-ts-0.59/src/main.tsx
@@ -25,7 +25,8 @@ type Props = ReduxState & { dispatch: Dispatch };
 class MainLayout extends NavigatorLayout<Props> {
   /**
    *
-   * @desc hookRoute 可以在这里针对特定路由做一些控制处理
+   * @desc hookRoute 可以在这里针对特定路由做一些控制处理，
+   * 具体可控制的参数可参考 NavigationOptions 类型描述
    */
   hookRoute(route: DeprecatedNavigatorRoute): NavigationOptions {
     const routeProps: NavigationOptions = {};
@@ -51,11 +52,15 @@ class MainLayout extends NavigatorLayout<Props> {
 
   /**
    * @desc
-   * 在此可以通过route中的id来判断使用哪个页面组件，
-   * 此外如果有额外的props需要传递给页面组件的，可以在此进行传递。
+   * 在此您可以通过 route 中的 ID 来判断使用哪个页面组件
+   * 如果有额外的 props 需要传递给页面组件的，可以在此进行传递
+   * 注意：route 参数来自于 TYSdk.Navigator.push，
+   * 如果调用了 TYSdk.Navigator.push({ id: 'setting', title: 'Setting Page' });
+   * 则会在推入路由栈时 hookRoute 和 renderScene 这个周期里会接受到 route = { id: 'setting', title: 'Setting Page' }，
+   * 但要注意的是，首页的 route 参数是固定的，为 { 'id': 'main', 'initialRoute': true }
    *
    * @param {Object} route - route对象
-   * @param {object} navigator - Navigator对象，具体使用方法可参考 https://facebook.github.io/react-native/docs/0.43/navigator.html
+   * @param {object} navigator - Navigator对象，具体使用方法可参考https://facebook.github.io/react-native/docs/0.43/navigator.html
    */
   renderScene(route: DeprecatedNavigatorRoute, navigator: DeprecatedNavigator) {
     let component;
