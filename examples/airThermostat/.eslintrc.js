490630089d338d9d66fa8a51035e0e3dc5d1d396
hulf <hulf@tuya.com>
2020-12-30 20:28:04 +0800

    feat: 调整项目配置

--- examples/airThermostat/.eslintrc.js
+++ examples/airThermostat/.eslintrc.js
@@ -1,64 +1,27 @@
+const path = require('path');
+
 module.exports = {
   root: true,
-  extends: ['./.eslintrc.base.js'],
+  extends: ['tuya/react-native'],
   rules: {
-    'import/no-unresolved': ['error', { ignore: ['^(react|react-native)$'] }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import/no-extraneous-dependencies': 0,
-    'react/jsx-filename-extension': 0,
-    'react/sort-comp': [
-      1,
-      {
-        order: [
-          'static-variables',
-          'static-methods',
-          'lifecycle',
-          '/^on.+$/',
-          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-          'everything-else',
-          '/^render.+$/',
-          'render',
-        ],
-      },
-    ],
+    'one-var': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'react/jsx-props-no-spreading': 0,
   },
   settings: {
-    'import/core-modules': ['@tuya-rn/tuya-native-web-context'],
     'import/resolver': {
-      'react-native': { platform: 'both' },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.ios.js', '.android.js'],
+      alias: {
+        map: [
+          ['api', path.resolve(__dirname, './src/api')],
+          ['components', path.resolve(__dirname, './src/components')],
+          ['config', path.resolve(__dirname, './src/config')],
+          ['i18n', path.resolve(__dirname, './src/i18n')],
+          ['models', path.resolve(__dirname, './src/models')],
+          ['res', path.resolve(__dirname, './src/res')],
+          ['utils', path.resolve(__dirname, './src/utils')],
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
       },
     },
   },
-  overrides: [
-    {
-      parser: '@typescript-eslint/parser',
-      files: ['**/*.ts', '**/*.tsx'],
-      env: { browser: true, es6: true, node: true },
-      extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: ['react', 'react-native', 'prettier', '@typescript-eslint'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/no-use-before-define': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/ban-ts-ignore': 0,
-      },
-    },
-  ],
 };
