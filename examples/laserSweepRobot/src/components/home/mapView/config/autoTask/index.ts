0933926237fb2b5c9c33a5d51c34bee4195490eb
huainan <huainan.luo@tuya.com>
2022-10-21 14:49:12 +0800

    feat: 移除实时地图OSS方案

--- examples/laserSweepRobot/src/components/home/mapView/config/autoTask/index.ts
+++ examples/laserSweepRobot/src/components/home/mapView/config/autoTask/index.ts
@@ -1,17 +1,9 @@
 export { default as updateAuthentication } from './updateAuthentication'; // done
 
-export { default as realTimeMap } from './realTimeMap';
-export { default as realTimeFirstMap } from './realTimeFirstMap'; // done
-export { default as realTimeFirstPath } from './realTimeFirstPath'; // done
-export { default as realTimeFullPath } from './realTimeFullPath';
-export { default as realTimePath } from './realTimePath';
 export { default as realTimeArea } from './realTimeArea';
-export { default as realTimePlanPath } from './realTimePlanPath';
 export { default as realTimeFullPathWithP2p } from './realTimeFullPathWithP2p';
 export { default as realTimePlanPathWithP2p } from './realTimePlanPathWithP2p';
 export { default as realTimeMapWithP2p } from './realTimeMapWithP2p';
 
-export { default as requestPath } from './requestPath';
 export { default as historyMap } from './historyMap';
 export { default as multiFloor } from './multiFloor';
-export { default as requestMap } from './requestMap';
